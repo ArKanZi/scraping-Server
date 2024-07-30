10902c1b903e3a59e464dcc4607c77c2d82a0268
ArKanZi <ayushku.ak@gmail.com>
2024-07-30 22:06:10 +0530

    new fix

--- src/services/doodsteamService.js
+++ src/services/doodsteamService.js
@@ -3,7 +3,7 @@ const { chromium } = require("playwright");
 async function doodstreamScraper(url) {
   const quality = "Doodstream";
   const browser = await chromium.launch({
-    headless: true,
+    headless: false,
   });
   const context = await browser.newContext();
   const page = await context.newPage();
@@ -13,13 +13,13 @@ async function doodstreamScraper(url) {
     path: `./src/images/screeenshot-${timestamp}.png`,
     fullPage: true,
   });
-  await page.getByRole("link", { name: "Download Now  􏁡" }).click();
+  await page.getByRole("link", { name: "Download Now" }).click();
   const newUrl = page.url();
   const hostname = new URL(newUrl).hostname;
   await page.waitForTimeout(5000);
-  await page.getByRole("link", { name: "High quality  􏃭" }).click();
+  await page.getByRole("link", { name: "High quality" }).click();
   const videoUrl = await page
-    .getByRole("link", { name: "Download file  􏃭" })
+    .getByRole("link", { name: "Download file" })
     .getAttribute("href");
   await browser.close();
   function doodHeaders(host) {
